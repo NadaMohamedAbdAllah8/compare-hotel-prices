b695e8b0e7dfd53ae200164f6676d9673828bb28
nada <nada.mohamed@extech.com>
2022-11-01 21:04:00 +0200

    read advertisers

---
+++ api/objects/advertiser.php
@@ -0,0 +1,42 @@
+<?php
+
+class Advertiser
+{
+
+    // database connection and table name
+    private $conn;
+    private $table_name = "advertisers";
+
+    // object properties
+    public $id;
+    public $name;
+    public $url;
+    public $method;
+    public $created;
+
+    // constructor with $db as database connection
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    // read advertisers
+    public function read()
+    {
+
+        // select all query
+        $query = "SELECT 'id', 'name', 'url', 'method'  FROM " . $this->table_name .
+            "  ORDER BY created DESC";
+
+        echo ($query);
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+}
