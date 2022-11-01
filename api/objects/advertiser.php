65209e54b921456a2f8817d0dd683972bc71f815
nada <nada.mohamed@extech.com>
2022-11-01 21:53:16 +0200

    delete advertiser

--- api/objects/advertiser.php
+++ api/objects/advertiser.php
@@ -37,7 +37,7 @@ class Advertiser
         return $stmt;
     }
 
-    // create product
+    // create advertiser
     public function create()
     {
 
@@ -72,6 +72,30 @@ class Advertiser
 
         return false;
 
+    }
+
+    // delete the advertiser
+    public function delete()
+    {
+
+        // delete query
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // bind id of record to delete
+        $stmt->bindParam(1, $this->id);
+
+        // execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
     }
 
 }
